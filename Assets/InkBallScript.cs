82fdd59ca6ff744ffd2a580562198d128e50d09f
Leon Lei <leon_lei@brown.edu>
2018-02-24 18:01:41 -0500

    First functionality demo

---
+++ Assets/InkBallScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InkBallScript : MonoBehaviour
+{
+
+    public int moveSpeed = 10;
+    Vector3 direction;
+
+    // Use this for initialization
+    void Start()
+    {
+        Physics2D.IgnoreLayerCollision(gameObject.layer, 8, true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Vector3 v = new Vector3(0, 1, 0);
+        transform.Translate(direction * Time.deltaTime * moveSpeed);
+    }
+
+    public void setDirection(Vector2 dir)
+    {
+        direction = new Vector2(dir.x, dir.y);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.layer == 9)
+        {
+            Destroy(gameObject, 0.05f);
+        }
+    }
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.layer == 9 || collider.gameObject.layer == 11)
+        {
+            Destroy(gameObject, 0.05f);
+        }
+    }
+
+}
---
+++ Assets/PlayerShoot.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour
+{
+    public GameObject inkBall;
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            Fire();
+        }
+    }
+
+    void Fire()
+    {
+        var ball = (GameObject)Instantiate(
+            inkBall, this.transform.position, inkBall.transform.rotation);
+    }
+
+}
