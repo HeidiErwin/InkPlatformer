bfbef9bec688f0441ab095bd0dc5de95d3bff55c
ashleykim805 <ashley_kim2@brown.edu>
2018-03-16 03:43:20 -0400

    Add files via upload

---
+++ Assets/Scripts/SpikeNormal.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeNormal : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.layer == 8) { // if colliding with player
+			Player p = other.gameObject.GetComponent<Player>(); // get the Player component of our Player object
+			p.Death(); // kill the player
+		}
+	}
+}
