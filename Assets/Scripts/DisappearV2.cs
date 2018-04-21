5673d988e557573b0657ff3e7e1922b10dbb3141
Leon Lei <leon_lei@brown.edu>
2018-04-21 16:57:22 -0400

    added waterfall

--- Assets/Scripts/DisappearV2.cs
+++ Assets/Scripts/DisappearV2.cs
@@ -6,11 +6,13 @@ public class DisappearV2 : MonoBehaviour
 {
     private SpriteRenderer m_SpriteRenderer;
     private bool solidified = false;
-
+    public bool dropWater = true;
+    public float dropSpeed = 0.2f;
     private float dripTimer;
     private bool timerRunning;
     public float dripTimerMax = 3f;
     public GameObject inkBallPrefab;
+    public GameObject waterPrefab;
 
     void Start()
     {
@@ -20,6 +22,11 @@ public class DisappearV2 : MonoBehaviour
         solidified = false;
         gameObject.layer = 11; // empty layer
         m_SpriteRenderer.color = Color.red;
+        if (dropWater)
+        {
+            InvokeRepeating("DropWater", 0.5f, dropSpeed);
+        }
+
     }
 
     void PlatformTimer()
@@ -34,6 +41,10 @@ public class DisappearV2 : MonoBehaviour
             solidified = false;
             gameObject.layer = 11; // empty layer
             m_SpriteRenderer.color = Color.red;
+            if (dropWater)
+            {
+                InvokeRepeating("DropWater", 0.5f, dropSpeed);
+            }
         }
     }
 
@@ -48,11 +59,25 @@ public class DisappearV2 : MonoBehaviour
         b.GetComponent<InkBallScript>().SetDirection(dir);
     }
 
+    void DropWater()
+    {
+        Quaternion ray = new Quaternion(0, -1, 0, 0);
+        Vector2 dir = new Vector2(0, -1);
+        Vector3 pos = gameObject.transform.position;
+        float boxOffset = gameObject.transform.lossyScale.y * gameObject.GetComponent<BoxCollider2D>().size.y / 2;
+        float waterOffset = waterPrefab.GetComponent<BoxCollider2D>().size.y * waterPrefab.transform.lossyScale.y / 2;
+        pos.y = pos.y - gameObject.transform.localScale.y - waterOffset - boxOffset - 0.01f;
+        var b = (GameObject)Instantiate(waterPrefab, pos, ray);
+        b.GetComponent<InkBallScript>().SetDirection(dir);
+
+    }
+
     void OnTriggerEnter2D(Collider2D other)
     {
         // the Ink's layer
         if (other.gameObject.layer == 10)
         {
+            CancelInvoke();
             timerRunning = true;
             m_SpriteRenderer.color = Color.black;
             gameObject.layer = 9; // solidified block layer
