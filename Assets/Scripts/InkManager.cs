3c1e03d4ebc3ba6c58bbf935348f40b0e1419e72
Fawn <fangrui_tong@brown.edu>
2018-04-21 16:16:26 -0400

    inkwell ui under Fawn scene

--- Assets/Scripts/InkManager.cs
+++ Assets/Scripts/InkManager.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InkManager : MonoBehaviour {
 
     public static InkManager Instance;
-
+	public Slider inkSlider;
+	public Text inkCounter;
     public float InkMax = 10;
     public float InkRemaining { get; private set; }
 
@@ -21,11 +23,14 @@ public class InkManager : MonoBehaviour {
         }
 
         InkRemaining = InkMax;
+		inkCounter.text = InkMax.ToString();
     }
 
 	public void DecrementInk()
     {
         InkRemaining--;
+		inkSlider.value = InkRemaining;
+		inkCounter.text = InkRemaining.ToString();
     }
 
     public void Reset()
