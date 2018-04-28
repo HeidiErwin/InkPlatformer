2f74fa931e8001b0eba4900b6a55cd9f71028ebc
Fawn <fangrui_tong@brown.edu>
2018-04-28 15:54:21 -0400

    inkwell prefab

---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour {
+
+	public GameObject Canvas;
+	public static bool Paused = false;
+
+	void Start(){
+		Canvas.gameObject.SetActive (false);
+	}
+
+	void Update () {
+		if (Input.GetKeyDown ("escape")) {
+			if(Paused == true){
+				Time.timeScale = 1.0f;
+				Canvas.gameObject.SetActive (false);
+				Paused = false;
+			} else {
+				Time.timeScale = 0.0f;
+				Canvas.gameObject.SetActive (true);
+				Paused = true;
+			}
+		}
+	}
+		
+}
